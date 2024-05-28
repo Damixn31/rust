d34a6315be42d2b4f63e41d5fd0a75c887abad85
Damixn31 <damianol29@hotmail.com>
2024-05-28 17:09:16 -0300

    agregado de comando

--- proyects/send_email/src/email/create_email.rs
+++ proyects/send_email/src/email/create_email.rs
@@ -4,30 +4,50 @@ use lettre::Message;
 
 use crate::config::read_config;
 
-pub fn create_email(destinatario: &str) -> Message {
+pub fn create_email(destiny: &str, message_html: &str, pdf_path: Option<&str>) -> Message {
     let conf = read_config();
     let from_email = format!("Damian Olmedo <{}>", conf.email);
-    let message_html = String::from(
-        "<h1>Helowww</h1>\n 
-        manu estoy sin whasap estoy en discord por cualquier cosa",
+
+    // let message_html = String::from(
+    //     "<h1>Helowww</h1>\n
+    //     manu estoy sin whasap estoy en discord por cualquier cosa",
+    // );
+    let mut multipart = MultiPart::mixed().singlepart(
+        SinglePart::builder()
+            .header(ContentType::TEXT_HTML)
+            .body(message_html.to_string()),
     );
-    let name_file = String::from("Documento sin titulo-v1.pdf");
-    let content_file = std::fs::read(conf.cv).unwrap();
-    let attach = Attachment::new(name_file)
-        .body(content_file, ContentType::parse("application/pdf").unwrap());
+
+    // let name_file = String::from("Documento sin titulo-v1.pdf");
+    // let content_file = std::fs::read(conf.cv).unwrap();
+    // let attach = Attachment::new(name_file)
+    //     .body(content_file, ContentType::parse("application/pdf").unwrap());
+
+    if let Some(path) = pdf_path {
+        let content_file = std::fs::read(path).unwrap();
+        let attach = Attachment::new("Documento adjunto.pdf".to_string())
+            .body(content_file, ContentType::parse("application/pdf").unwrap());
+        multipart = multipart.singlepart(attach);
+    }
 
     Message::builder()
         .from(from_email.parse().unwrap())
-        .to(destinatario.parse().unwrap())
+        .to(destiny.parse().unwrap())
         .subject("enviando CV!")
-        .multipart(
-            MultiPart::mixed()
-                .singlepart(
-                    SinglePart::builder()
-                        .header(ContentType::TEXT_HTML)
-                        .body(message_html),
-                )
-                .singlepart(attach),
-        )
+        .multipart(multipart)
         .unwrap()
+    // Message::builder()
+    //     .from(from_email.parse().unwrap())
+    //     .to(destinatario.parse().unwrap())
+    //     .subject("enviando CV!")
+    //     .multipart(
+    //         MultiPart::mixed()
+    //             .singlepart(
+    //                 SinglePart::builder()
+    //                     .header(ContentType::TEXT_HTML)
+    //                     .body(message_html),
+    //             )
+    //             .singlepart(attach),
+    //     )
+    //     .unwrap()
 }
--- proyects/send_email/src/menu/menu_use.rs
+++ proyects/send_email/src/menu/menu_use.rs
@@ -5,21 +5,30 @@ use crate::email::{create_email::create_email, send_email::send_email};
 pub fn print_use() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        println!("Uso: {} <comando>", args[0]);
+    if args.len() < 3 {
+        println!(
+            "Uso: {} <comando> <destinatario> [mensaje] [ruta_pdf]",
+            args[0]
+        );
         println!("Comandos disponibles");
-        println!(" enviar <destinatario>: Envia el correo electronico al destinatario.");
+        println!(" enviar <destinatario> [mensaje] [ruta_pdf]: Envia el correo electronico al destinatario el pdf es opcional.");
         return;
     }
 
     match args[1].as_str() {
         "enviar" => {
-            if args.len() != 3 {
+            if args.len() != 4 {
                 println!("Uso: {} enviar <destinatario>", args[0]);
                 return;
             }
-            let destinatario = &args[2];
-            let email = create_email(destinatario);
+            let destiny = &args[2];
+            let message = &args[3];
+            let path_pdf = if args.len() > 4 {
+                Some(args[4].as_str())
+            } else {
+                None
+            };
+            let email = create_email(destiny, message, path_pdf);
             send_email(&email);
         }
         _ => {
